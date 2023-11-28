a89bcf50c087a26e1b48b6b523030ab8a066ea83
MarceloVichar <marcos.marcelo43@gmail.com>
2023-11-28 09:20:22 -0300

    aula 27 11

---
+++ php-crud-compose2/db/crud.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE crud;
+
+USE crud;
+
+CREATE TABLE employees (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    salary INT(10) NOT NULL
+);
+
+GRANT ALL PRIVILEGES ON crud.* TO 'root'@'localhost' IDENTIFIED BY 'abc123';
+FLUSH PRIVILEGES;
